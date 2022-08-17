076239b2c081e40b28203e63714f48da658c8f90
Elozino <ovedhee@gmail.com>
2022-08-17 02:21:01 +0100

    added db

--- node-express/app.js
+++ node-express/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // Initializing Express
 const app = express();
@@ -6,20 +9,69 @@ const app = express();
 // Register view engine
 app.set("view engine", "ejs");
 
-// Listening for request
-app.listen(8000);
+// middleware
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://Elozino:nodetest1@cluster0.yuqoj.mongodb.net/node-learning?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(8000))
+  .catch((err) => console.log(err));
+
+// // Listening for request
+// app.listen(8000);
+
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "Test Blog 2",
+    snippet: "This is a test blog",
+    body: "This is the body of the test blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("62fc414d7ba1abc66353bae2")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("index", {title: "Home"});
+  res.render("index", { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {title: "About"});
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create", {title: "Create a new blog"});
+  res.render("create", { title: "Create a new blog" });
 });
 
 //redirect
@@ -29,5 +81,5 @@ app.get("/blogs/create", (req, res) => {
 
 //404 page
 app.use((req, res) => {
-  res.status(404).render("404", {title: "404"});
+  res.status(404).render("404", { title: "404" });
 });
